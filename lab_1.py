cf6d3102aa9510f504c7031cc944d563ccd6b004
abdlaz12 <153602120+abdlaz12@users.noreply.github.com>
2024-03-13 11:18:01 +0700

    Update lab_1.py
    ada beberapa perubahan dalam code

--- lab_1.py
+++ lab_1.py
@@ -1,7 +1,19 @@
-kata_rahasia= "Opungku"
-tabakanmu = []
+kata_rahasia= "software"
+tebakanmu = []
 kesempatanmu = 6
-while chances > 0:
+while kesempatanmu > 0:
+    display = ""
+    for kata in kata_rahasia:
+        if kata.lower() in tebakanmu:
+            display += kata
+        else:
+            display += "_"
+    print(display)
+    if "_" not in display:
+        print("Salamat kamu benar: ", kata_rahasia)
+    if kesempatanmu == 0:
+        print("Sorry kesempatan kamu dah abis, anjing mu mati: Depo dulu", kata_rahasia)
+    
     tebak = input("Tabaklah kata rahasiaku kawan: ")
     if len(tebak) != 1:
         print("Masukkan minimal satu huruf")
@@ -16,19 +28,7 @@ while chances > 0:
     else:
         kesempatanmu -= 1
         print("Semoga kamu beruntung. Kamu ada {} kesempatan lagi.".format(kesempatanmu))
-    display = ""
-    for kata in kata_rahasia:
-            if kata.lower() in tebakanmu:
-                display += kata
-            else:
-                display += "_"
-                print(display)
-                if "_" not in display:
-                    print("Salamat kamu benar: ", kata_rahasia)
-                if kesempatanmu == 0:
-                    print("Sorry kesempatan kamu dah abis, anjing mu mati: Depo dulu", kata_rahasia)
-
-    
+   
 # kata_rahasia='taik'
 # tebakan=[]
 # kesempatan=6
