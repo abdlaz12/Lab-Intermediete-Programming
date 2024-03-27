bb021fa181de52ef6b55f5b1a29e5236713b6ed1
abdlaz12 <153602120+abdlaz12@users.noreply.github.com>
2024-03-27 13:07:23 +0700

    Update lab_1.py

--- lab_1.py
+++ lab_1.py
@@ -1,32 +1,18 @@
-kata_rahasia= "software"
+kata_rahasia= "brody"
 tebakanmu = []
-kesempatanmu = 20
-while kesempatanmu > 0:
-    if kesempatanmu == 0:
-        print("Sorry kesempatan kamu dah abis, anjing mu mati: Depo dulu", kata_rahasia)
+kesempatanmu = 6
+while True:
     
     tebak = input("Tabaklah kata rahasiaku kawan: ")
     if len(tebak) != 1:
         print("Masukkan minimal satu huruf")
     elif not tebak.isalpha():
         print("Kasih aku huruf sobat")
-    elif tebak.lower() in tebakanmu:
+    if tebak.lower() in tebakanmu:
         print("kau dah tebak ituu")
     else:
         tebakanmu.append(tebak.lower())
-    if tebak.lower() in kata_rahasia.lower():
-        print("Salamat kamu benar sobat")
-    else:
-        kesempatanmu -= 1
-        print("Semoga kamu beruntung. Kamu ada {} kesempatan lagi.".format(kesempatanmu))
-   
-# kata_rahasia='taik'
-# tebakan=[]
-# kesempatan=6
-# while kesempatan>0:
-#     tebak=input('Masukkan tebakanmu')
-#     if tebak!=kata_rahasia:
-#         kesempatan-=1
-#         print('kamu salah coba lagi')
-#     else:
-#         print('kamu benar yeee')
+        if tebak.lower() in kata_rahasia.lower():
+            print("Salamat kamu benar sobat")
+        elif kata_rahasia == tebakanmu:
+            break
