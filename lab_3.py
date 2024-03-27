775f44c73aafed5c10c1594406bee03c19c442c2
abdlaz12 <153602120+abdlaz12@users.noreply.github.com>
2024-03-27 11:03:49 +0700

    Update lab_3.py
    ada sedikit error karena human error

--- lab_3.py
+++ lab_3.py
@@ -13,27 +13,28 @@ def Update_letter(kata_rahasia, tebakanmu):
 kata_rahasia= "nyala"
 tebakanmu = []
 kesempatanmu = 6
-
-
-while kesempatanmu > 0:
-    tebak = input("Tabaklah kata rahasianya kawan: ")
+while kesempatanmu>0:
+    tebak=input('Masukkan tebakanmu kawan: ')
     if len(tebak) != 1:
-        print("Masukkan minimal satu huruflah")
+        print("Masukkan maximal satu huruflah")
     elif not tebak.isalpha():
         print("Kasih aku huruf sobat")
-    elif tebak.lower() in tebakanmu:
-        print("kau dah tebak ituu")
+    if tebak.lower() in tebakanmu:
+            print("kau dah tebak ituu")
     else:
         tebakanmu.append(tebak.lower())
         if tebak.lower() in kata_rahasia.lower():
-            print("Salamat kamu benar sobat")
+            print("Salamat kamu benar sobat") 
         else:
             kesempatanmu -= 1
             print("Semoga kamu beruntung. Kamu ada {} kesempatan lagi.".format(kesempatanmu))
-    display=Update_letter(kata_rahasia, tebakanmu)
-    print(display)
-    if tebak == kata_rahasia:
-        print("Selamat kamu benar. kamu berhasil nyawa kamu ada {}".format(kesempatanmu))
-if kesempatanmu==0:
-    print(f"Maaf kamu telah kalah. Jawabannya{kata_rahasia}")
+        display=Update_letter(kata_rahasia, tebakanmu)
+    print(display) 
 
+    if kata_rahasia == display :
+        print('Horeee, we did it mas bro')
+        break
+if kesempatanmu==0:
+    print(f"Maaf kamu telah kalah. Jawabannya {kata_rahasia}")
+if kata_rahasia in tebakanmu:
+    print(f"Selamat kamu benar. kamu berhasil nyawa kamu ada {tebakanmu}")
