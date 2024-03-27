dc27a4b965c08bb40070fd9e459829fcbde71f80
abdlaz12 <153602120+abdlaz12@users.noreply.github.com>
2024-03-27 11:54:29 +0700

    Update lab_2.py

--- lab_2.py
+++ lab_2.py
@@ -1,4 +1,4 @@
-kata_rahasia= "software"
+kata_rahasia= "loveu"
 tebakanmu = []
 kesempatanmu = 6
 while kesempatanmu > 0:
@@ -11,35 +11,22 @@ while kesempatanmu > 0:
     print(display)
     if "_" not in display:
         print("Salamat kamu benar: ", kata_rahasia)
-    if kesempatanmu == 0:
-        print("Sorry kesempatan kamu dah abis, anjing mu mati: Depo dulu", kata_rahasia)
-    
-    tebak = input("Tabaklah kata rahasiaku kawan: ")
+        print('Horeee, we did it mas bro')
+        break
+    tebak = input("Tebaklah kata rahasiaku kawan: ")
     if len(tebak) != 1:
         print("Masukkan minimal satu huruf")
     elif not tebak.isalpha():
         print("Kasih aku huruf sobat")
-    elif tebak.lower() in tebakanmu:
+    if tebak.lower() in tebakanmu:
         print("kau dah tebak ituu")
     else:
         tebakanmu.append(tebak.lower())
-    if tebak.lower() in kata_rahasia.lower():
-        print("Salamat kamu benar sobat")
-    else:
-        kesempatanmu -= 1
-        print("Semoga kamu beruntung. Kamu ada {} kesempatan lagi.".format(kesempatanmu))
-    if tebak == kata_rahasia:
-        print("Selamat kamu benar. kamu berhasil nyawa kamu ada {}".format(kesempatanmu))
-        break 
-
-   
-# kata_rahasia='taik'
-# tebakan=[]
-# kesempatan=6
-# while kesempatan>0:
-#     tebak=input('Masukkan tebakanmu')
-#     if tebak!=kata_rahasia:
-#         kesempatan-=1
-#         print('kamu salah coba lagi')
-#     else:
-#         print('kamu benar yeee')
+        if tebak.lower() in kata_rahasia.lower():
+            print("Salamat kamu benar sobat")   
+        else:
+            kesempatanmu -= 1
+            print("Semoga kamu beruntung. Kamu ada {} kesempatan lagi.".format(kesempatanmu))
+if kesempatanmu == 0:
+        print(f"Sorry kesempatan kamu dah abis, ini jawabannya {kata_rahasia}")
+       
